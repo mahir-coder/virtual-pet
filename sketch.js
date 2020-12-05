f01ac392019b4d18c8d79871bd34bc9f2844c682
mahir-coder <71507226+mahir-coder@users.noreply.github.com>
2020-12-05 12:55:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+//Create variables here
+var dog,doghappy
+var foodS,foodStock
+var database
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage("images/dogImg.png")
+}
+
+function setup() {
+  createCanvas(500,500);
+  dog = createSprite(250,250,50,50)
+  dog.visible = false
+  
+  foodStock=database.ref('Food');
+   foodStock.on("value",readStock )
+  
+}
+
+
+function draw() {  
+imageMode(CENTER)
+  drawSprites();
+  //add styles here
+  image(dogImg,dog.x,dog.y,dog.width,dog.height)
+  
+
+}
+
+
+
