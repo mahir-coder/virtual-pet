c31f8dfbdd170594e0f200b82472430f7ad0290c
mahir-coder <71507226+mahir-coder@users.noreply.github.com>
2020-12-11 17:52:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,13 +7,17 @@ function preload()
 {
   //load images here
   dogImg = loadImage("images/dogImg.png")
+  dogImg2 = loadImage("images/dogImg1.png")
 }
 
 function setup() {
   createCanvas(500,500);
   database = firebase.database();
   dog = createSprite(250,250,50,50)
-  dog.visible = false
+  dog.addImage(dogImg)
+  dog.scale=(0.3)
+ 
+
   
   foodStock=database.ref('Food');
    foodStock.on("value",readStock )
@@ -22,11 +26,32 @@ function setup() {
 
 
 function draw() {  
+  background(46,139,87)
 imageMode(CENTER)
   drawSprites();
   //add styles here
-  image(dogImg,dog.x,dog.y,dog.width,dog.height)
   
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS)
+    dog.addImage(dogImg2)
+  }
+  
+
+}
+function readStock(data){
+foodS = data.val()
+}
+function writeStock(x){
+  if(x<0){
+    x = 0
+  }else{
+    x=x-1
+  }
+  database.ref('/').update({
+Food:x
+  })
+}
+function keyPressed(){
 
 }
 
